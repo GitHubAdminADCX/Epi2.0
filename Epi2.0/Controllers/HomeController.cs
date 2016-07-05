ce89d6d889ac79edda3ccab32eb27e053f65e860
Amol Mahul <amol.mahul@capgemini.com>
2016-07-05 08:37:20 +0000

    Added standard templates

---
+++ Epi2.0/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using Epi2._0.Models.Pages;
+
+namespace Epi2._0.Controllers
+{
+    public class HomeController : PageController<StandardPageType>
+    {
+        public ActionResult Index(StandardPageType currentPage)
+        {
+            /* Implementation of action. You can create your own view model class that you pass to the view or
+             * you can pass the page type for simpler templates */
+
+            return View(currentPage);
+        }
+    }
+}
---
+++ Epi2.0/Models/Pages/StandardPageType.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace Epi2._0.Models.Pages
+{
+    [ContentType(DisplayName = "StandardPageType", GUID = "566c8a3a-be39-467f-a8c6-5230701b83d9", Description = "This is a standard page type model")]
+    public class StandardPageType : PageData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
