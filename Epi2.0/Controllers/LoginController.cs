0b6caffcbff0aeab7888a32c8c8bd24a3630dc85
Rushabh Shah <rushabh.d.shah@capgemini.com>
2016-07-18 11:00:04 +0000

    Login Logout session changes

---
+++ Epi2.0/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Epi2._0.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebClient.Views.Blocks;
+
+namespace Epi2._0.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginModel lm, DefaultBlock1 d)
+        {
+            if (ModelState.IsValid)
+            {
+                if (lm.UserName == "admin" && lm.Password == "admin")
+                {
+                    var userCookie = new HttpCookie("UserLoggedin");
+                    userCookie.Expires.AddDays(365);
+                    HttpContext.Response.Cookies.Add(userCookie);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "invalid username or password");
+
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Epi2.0/Views/Blocks/DefaultBlock1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+
+namespace WebClient.Views.Blocks
+{
+    [ContentType(DisplayName = "DefaultBlock1", GUID = "134637cf-aeac-4c00-9ff7-2c39ba335073", Description = "")]
+    public class DefaultBlock1 : BlockData
+    {
+        /*
+                [CultureSpecific]
+                [Display(
+                    Name = "Name",
+                    Description = "Name field's description",
+                    GroupName = SystemTabNames.Content,
+                    Order = 1)]
+                public virtual string Name { get; set; }
+         */
+    }
+}
