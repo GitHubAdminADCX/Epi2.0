b91b18b19be499074bca7304b71bb358e1f9c68b
Amol Mahul <amol.mahul@capgemini.com>
2016-07-19 09:55:43 +0000

    Task 1:Validation Property changes Task 2:Preview Renderer added

--- Epi2.0/Models/Pages/StandardPageType.cs
+++ Epi2.0/Models/Pages/StandardPageType.cs
@@ -4,6 +4,8 @@ using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
 using EPiServer.SpecializedProperties;
+using Epi2._0.Models.Custom.Attributes;
+using EPiServer;
 
 namespace WebClient.Models.Pages
 {
@@ -17,7 +19,28 @@ namespace WebClient.Models.Pages
             Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
             GroupName = SystemTabNames.Content,
             Order = 1)]
+        [ValidationProperty]
         public virtual XhtmlString MainBody { get; set; }
+
+        [CultureSpecific]
+        [Display(
+          Name = "Content Area",
+          Description = "",
+          GroupName = SystemTabNames.Content,
+          Order = 25)]
+        public virtual ContentArea TestContentArea { get; set; }
+
+        [CultureSpecific]
+        [Display(
+            Name = "Website url",
+            Description = "",
+            GroupName = SystemTabNames.Content,
+            Order = 91)]
+        [ValidationProperty]
+        [BackingType(typeof(PropertyUrl))]
+        public virtual Url WebSiteUrl { get; set; }
+
+
 
     }
 }
