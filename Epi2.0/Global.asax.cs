81e5a996b7d917c950659efc9035ec3006df1fb6
Amol Pawar <amol.pawar@capgemini.com>
2016-07-19 09:16:38 +0200

    Global File changes done

--- Epi2.0/Global.asax.cs
+++ Epi2.0/Global.asax.cs
@@ -9,16 +9,10 @@ namespace WebClient
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
             //Tip: Want to call the EPiServer API on startup? Add an initialization module instead (Add -> New Item.. -> EPiServer -> Initialization Module)
         }
 
-        protected override void RegisterRoutes(RouteCollection routes)
-        {
-            base.RegisterRoutes(routes);
-            routes.MapRoute("myadminplugin", "MyAdminPlugin/{action}",
-            new { controller = "MyAdminPlugin", action = "Index" });
-        }
         public static class SiteUIHints
         {
             public const string Contact = "contact";
