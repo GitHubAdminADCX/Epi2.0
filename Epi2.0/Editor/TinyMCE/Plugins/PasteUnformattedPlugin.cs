c9a500427223970ce5d785aa7d3aefe4a034673f
Snehal Jadhav <snehal.b.jadhav@capgemini.com>
2016-07-27 16:15:01 +0200

    tiny mce plugin edited

---
+++ Epi2.0/Editor/TinyMCE/Plugins/PasteUnformattedPlugin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer.Editor.TinyMCE;
+
+namespace WebClient.Editor.TinyMCE.Plugins
+{
+    /// <summary>
+    /// Author: Snehal Jadhav
+    /// Purpose of the Class : Creates a non visual plugin in tiny mce plugin in Episerver Admin to paste text unformatted.
+    /// How to use: Add the control from the custom setting of the XHTMLString property    
+    /// Points to take care: will have a editor_plugin js file. 
+    /// </summary>
+    [TinyMCEPluginNonVisual(
+          AlwaysEnabled = true,
+          PlugInName = "PasteUnformattedPlugin",
+          DisplayName = "Custom editor paste unformatted",
+          Description = "Loads custom editor paste unformatted option",         
+          DefaultEnabled = true,
+          EditorInitConfigurationOptions = @"{
+            paste_text_sticky: true,
+            paste_text_sticky_default: true,
+			theme_advanced_disable: 'code',
+            forced_root_block : 'p',
+            force_br_newlines : true,
+            force_p_newlines : true,
+            convert_newlines_to_brs : false
+          }"
+          )]
+    public class PasteUnformattedPlugin
+    {
+    }
+}
--- Epi2.0/Util/Editor/tinymce/plugins/MyPlugin/editor_plugin.js
+++ Epi2.0/Util/Editor/tinymce/plugins/MyPlugin/editor_plugin.js
@@ -1,10 +1,17 @@
-﻿(function () {
-    tinymce.create('tinymce.plugins.MyPlugin', {
-        /*
-        * Initializes the plugin, this will be executed after the plugin has been created.
-        * This call is done before the editor instance has finished it's initialization so use the onInit event
-        */
+﻿/*
+Author: Snehal Jadhav
+Purpose of the Class : Creates a button in tiny mce plugin in Episerver Admin
+How to use: Add the control from the custom setting of the XHTMLString property  
+Points to take care: will have cs file with TinyMceButtonPlugin attribute
+*/
+(function () {
+
+    tinymce.create('tinymce.plugins.PasteUnformattedPlugin', {
         init: function (ed, url) {
+        /*
+            * Initializes the plugin, this will be executed after the plugin has been created.
+            * This call is done before the editor instance has finished it's initialization so use the onInit event
+        */
             if (!ed.plugins.paste)
                 return;
             ed.onInit.add(function (ed) {
@@ -14,6 +21,7 @@
         }
     });
 
-    tinymce.PluginManager.add('PasteUnformatted', tinymce.plugins.MyPlugin);
+    tinymce.PluginManager.add('PasteUnformattedPlugin', tinymce.plugins.PasteUnformattedPlugin);
 
 }());
+
