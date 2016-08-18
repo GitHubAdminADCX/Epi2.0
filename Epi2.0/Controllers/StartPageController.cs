006a1540643d896eb676038ae7849802ac7472a3
Amol Mahul <amol.mahul@capgemini.com>
2016-08-18 11:06:41 +0200

    Added StartPage Type

---
+++ Epi2.0/Controllers/StartPageController.cs
@@ -0,0 +1,21 @@
+﻿using EPiServer.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebClient.Models.Pages;
+using WebClient.Models.ViewModels;
+
+namespace WebClient.Controllers
+{
+    public class StartPageController : PageController<StartPageType>
+    {
+        // GET: StartPage
+        public ActionResult Index(StartPageType currentPage)
+        {
+            var model = PageViewModel.Create(currentPage);
+            return View(model);
+        }
+    }
+}
