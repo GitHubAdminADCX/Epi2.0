3ef7fde097687edbb0ddc4da49f431cb2d1c657c
Amol Mahul <amol.mahul@capgemini.com>
2016-08-22 11:11:52 +0200

    Adding home page styling

---
+++ Epi2.0/Models/Media/ImageFile.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Framework.DataAnnotations;
+
+namespace WebClient.Models.Media
+{
+    [ContentType(DisplayName = "ImageFile", GUID = "c7cd6751-6ccb-4f85-8808-f79e38869ba9", Description = "")]
+    [MediaDescriptor(ExtensionString = "jpg,jpeg,jpe,ico,gif,bmp,png")]
+    public class ImageFile : ImageData
+        {
+            /// <summary>
+            /// Gets or sets the copyright.
+            /// </summary>
+            /// <value>
+            /// The copyright.
+            /// </value>
+            public virtual string Image { get; set; }
+        }
+}
