028e9aa00104095d86452af4cd8e94a49494793e
Kunal <kunal.doshi@capgemini.com>
2016-08-26 10:07:29 +0000

    FRM023 - Complex Property

---
+++ Epi2.0/HelperClass/ComplexProperty/LocationBlockEditorDescriptor.cs
@@ -0,0 +1,21 @@
+﻿using EPiServer.Shell.ObjectEditing;
+using EPiServer.Shell.ObjectEditing.EditorDescriptors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebClient.Models.Blocks;
+
+namespace WebClient.HelperClass.ComplexProperty
+{
+    [EditorDescriptorRegistration(TargetType = typeof(LocationBlock))]
+    class LocationBlockEditorDescriptor : EditorDescriptor
+    {
+        public override void ModifyMetadata(EPiServer.Shell.ObjectEditing.ExtendedMetadata metadata, IEnumerable<Attribute> attributes)
+        {
+            base.ModifyMetadata(metadata, attributes);
+            metadata.Properties.Cast<ExtendedMetadata>().First().GroupSettings.ClientLayoutClass = "myproject.LocationBlockEditor.LocationBlockContainer";
+        }
+    }
+}
