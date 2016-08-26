b8f54d3232e773ca370fc60ca513b75180d77130
Priti jadhav <priti.jadhav@capgemini.com>
2016-08-26 14:04:06 +0000

    Added missing file

---
+++ Epi2.0/Models/Pages/AdminToolLocation.cs
@@ -0,0 +1,15 @@
+﻿using EPiServer.PlugIn;
+using EPiServer.Shell.WebForms;
+
+namespace WebClient.Models.Pages
+{
+    [GuiPlugIn(
+    DisplayName = "Admin Tool For Location",
+    RequiredAccess = EPiServer.Security.AccessLevel.Administer,
+    Area = PlugInArea.AdminMenu,
+    UrlFromModuleFolder = "/AdminToolLocation")]
+    public class AdminToolLocation : WebFormsBase
+    {
+        
+    }
+}
---
+++ Epi2.0/Models/ViewModels/AdminLocation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebClient.Models.ViewModels
+{
+    public class AdminLocation
+    {
+        public string CountryCode { get; set; }
+
+        public string CountryName { get; set; }
+
+        public string RegionName { get; set; }
+
+        public string RegionCode { get; set; }
+
+
+    }
+}
