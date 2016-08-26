366a8f11891b91a68e44f4f8fc382a0159c741b8
Yogesh Sandhokar <yogesh.sandhokar@capgemini.com>
2016-08-26 11:25:45 +0530

    Grunt updated

--- Epi2.0/GruntFile.js
+++ Epi2.0/GruntFile.js
@@ -2,17 +2,36 @@
 module.exports = function (grunt) {
     var path = require('path');
 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    // Loads grunt tasks defined in package.json
-    require('load-grunt-config')(grunt, {
-        configPath: path.join(process.cwd(), 'ui/grunt-config'), //path to task.js files, defaults to grunt dir
-        init: true, //auto grunt.initConfig
+        sass: {
+            dist: {
+                files: {
+                    'public/stylesheets/style.css': 'sass/style.scss'
+                }
+            }
+        },
+        watch: {
+            scripts: {
+                files: ['scripts/**/*.js'],
+                tasks: ['tags-debug'],
+                options: {
+                    livereload: true,
+                },
 
-        config: { //additional config vars
-            uiRoot: 'ui/'
+            },
+            source: {
+                files: ['sass/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    livereload: true, // needed to run LiveReload
+                }
+            }
         }
-    });
+    })
 
-    grunt.registerTask('default', ['build']);
-
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.registerTask('default', ['tags', 'sass']);
 };
